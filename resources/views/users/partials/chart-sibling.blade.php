7eabe900a26d5748db55b30f7d9aa948aa0e815a
Robie Hakim <116531348+robiehakim88@users.noreply.github.com>
2025-02-26 11:24:50 +0700

    Update chart-sibling.blade.php

--- resources/views/users/partials/chart-sibling.blade.php
+++ resources/views/users/partials/chart-sibling.blade.php
@@ -3,7 +3,27 @@
         <tbody>
             <tr>
                 <th style="width: 35%">{{ trans('user.siblings') }}</th>
-                <th class="text-center" colspan="{{ $sibling->childs->count() }}">{{ $sibling->profileLink('chart') }} ({{ $sibling->gender }})</th>
+                <th class="text-center" colspan="{{ $sibling->childs->count() }}">
+                    @if($sibling->photo_path)
+                <!--        <img src="{{ asset('storage/' . $sibling->photo_path) }}" class="person-photo" alt="Photo"><br>
+                -->    @endif
+                    {{ $sibling->profileLink('chart') }} ({{ $sibling->gender }})
+                    @if($sibling->couples->isNotEmpty())
+                        <br>
+                        <small>{{ trans('user.spouse') }}:
+                        @foreach($sibling->couples as $spouse)
+                            @if($spouse->photo_path)
+                <!--                <img src="{{ asset('storage/' . $spouse->photo_path) }}" class="person-photo" alt="Photo"><br>
+                -->            @endif
+                            {{ $spouse->profileLink('chart') }}
+                            @if($spouse->pivot->marriage_date)
+                                ({{ Carbon\Carbon::parse($spouse->pivot->marriage_date)->format('Y') }})
+                            @endif
+                            @if(!$loop->last), @endif
+                        @endforeach
+                        </small>
+                    @endif
+                </th>
             </tr>
             <tr>
                 <th>{{ trans('user.nieces') }} & {{ trans('user.grand_childs') }}</th>
@@ -11,10 +31,48 @@
                     <ol style="padding-left: 15px">
                         @foreach($sibling->childs as $child)
                         <li style="margin-top: 10px;">
+                            @if($child->photo_path)
+                    <!--            <img src="{{ asset('storage/' . $child->photo_path) }}" class="person-photo" alt="Photo"><br>
+                     -->       @endif
                             {{ $child->profileLink('chart') }} ({{ $child->gender }})
+                            @if($child->couples->isNotEmpty())
+                                <br>
+                                <small>{{ trans('user.spouse') }}:
+                                @foreach($child->couples as $spouse)
+                                    @if($spouse->photo_path)
+                        <!--                <img src="{{ asset('storage/' . $spouse->photo_path) }}" class="person-photo" alt="Photo"><br>
+                        -->            @endif
+                                    {{ $spouse->profileLink('chart') }}
+                                    @if($spouse->pivot->marriage_date)
+                                        ({{ Carbon\Carbon::parse($spouse->pivot->marriage_date)->format('Y') }})
+                                    @endif
+                                    @if(!$loop->last), @endif
+                                @endforeach
+                                </small>
+                            @endif
                             <ul style="padding-left: 18px">
                                 @foreach($child->childs as $grand)
-                                <li>{{ $grand->profileLink('chart') }} ({{ $grand->gender }})</li>
+                                <li>
+                                    @if($grand->photo_path)
+                        <!--                <img src="{{ asset('storage/' . $grand->photo_path) }}" class="person-photo" alt="Photo"><br>
+                        -->            @endif
+                                    {{ $grand->profileLink('chart') }} ({{ $grand->gender }})
+                                    @if($grand->couples->isNotEmpty())
+                                        <br>
+                                        <small>{{ trans('user.spouse') }}:
+                                        @foreach($grand->couples as $spouse)
+                                            @if($spouse->photo_path)
+                         <!--                       <img src="{{ asset('storage/' . $spouse->photo_path) }}" class="person-photo" alt="Photo"><br>
+                         -->                   @endif
+                                            {{ $spouse->profileLink('chart') }}
+                                            @if($spouse->pivot->marriage_date)
+                                                ({{ Carbon\Carbon::parse($spouse->pivot->marriage_date)->format('Y') }})
+                                            @endif
+                                            @if(!$loop->last), @endif
+                                        @endforeach
+                                        </small>
+                                    @endif
+                                </li>
                                 @endforeach
                             </ul>
                         </li>
